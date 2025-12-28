c70a7fa539fd6150925ddc0574c4b5e9903bfa14
piyapat <piyapat.thongrueng@gmail.com>
2025-12-28 14:10:57 +0700

    feat(navbar): update Navbar component with logo and login/signup buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
     </>
   );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,23 @@
-import { Camera } from "lucide-react";
+import weblogo from "../../assets/icons/web-logo.svg";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className="flex px-8 pt-8 pb-4 justify-between items-center border-b lg:px-20 lg:py-4">
+      <div>
+        <img src={weblogo} alt="web-logo" className="w-6 h-6 lg:w-11 lg:h-11" />
+      </div>
+      <div className="hidden md:flex gap-1.5">
+        <div className="border border-brown-400 bg-white text-brown-600 px-10 py-3 rounded-full">
+          <button>Login</button>
+        </div>
+        <div className="border border-brown-600 bg-brown-600 text-white px-10 py-3 rounded-full">
+          <button>Sign up</button>  
+        </div>
+      </div>
+      <div className="md:hidden">
+        <Menu size={19} />
+      </div>
     </nav>
   );
 };
